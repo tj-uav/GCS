7c7317407bac2efc3341bc802db753a6a8d0330e
JayChen35 <jay.chen135@gmail.com>
2019-05-06 18:03:17 -0400

    it works yay

--- missionplanner.py
+++ missionplanner.py
@@ -11,26 +11,26 @@ sock.connect(('127.0.0.1', 5005))
 print "Starting."
 while True:
     telem = {
-        'roll': cs.roll
-        'pitch': cs.pitch
-        'yaw': cs.yaw
-        'lat': cs.lat
-        'lon': cs.lng
-        'alt': cs.alt
-        'airspeed': cs.airspeed
-        'ground_speed': cs.groundspeed
-        'vertical_speed': cs.verticalspeed
-        'wind_dir': cs.wind_dir
-        'wind_vel': cs.wind_vel
-        'acc': cs.ax, cs.ay, cs.az
-        'gyr': cs.gx, cs.gy, cs.gz
-        'flying_mode': cs.mode
-        'battery_voltage': cs.battery_voltage
+        'roll': cs.roll,
+        'pitch': cs.pitch,
+        'yaw': cs.yaw,
+        'lat': cs.lat,
+        'lon': cs.lng,
+        'alt': cs.alt,
+        'airspeed': cs.airspeed,
+        'ground_speed': cs.groundspeed,
+        'vertical_speed': cs.verticalspeed,
+        'wind_dir': cs.wind_dir,
+        'wind_vel': cs.wind_vel,
+        'acc': (cs.ax, cs.ay, cs.az),
+        'gyr': (cs.gx, cs.gy, cs.gz),
+        'flying_mode': cs.mode,
+        'battery_voltage': cs.battery_voltage,
         'battery_remaining': cs.battery_remaining
     }
 
-    toSend = {"HEADER":"TELEMETRY", "MESSAGE":telem}
-    toBytes = json.dumps(toSend)
+    toSend = str(telem)
+    toBytes = toSend.encode('utf-8')
     sock.send(toBytes)
-    time.sleep(5)
+    time.sleep(.1)
     print "Sleeping for 5 seconds..."
