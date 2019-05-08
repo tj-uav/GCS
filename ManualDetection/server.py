7a761e631f17822d65d6d34a7fca0129e335726a
Srikar Gouru <srikarg89@gmail.com>
2019-05-08 19:21:05 -0400

    Working on Manual Detection Computer

--- ManualDetection/server.py
+++ ManualDetection/server.py
@@ -6,7 +6,10 @@ from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 import cv2
 import numpy as np
+import requests
 
+global x
+x = 1
 TCP_IP = '127.0.0.1'  # Need to change to IP of comms computer
 TCP_PORT = 5005
 BUFFER_SIZE = 1024  # Can make this lower if we need speed
@@ -14,24 +17,32 @@ BUFFER_SIZE = 1024  # Can make this lower if we need speed
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #sock.connect((TCP_IP, TCP_PORT))
 global image_num
-image_num = 0
+image_num = 20
 
-def save_image(image_string):
+def save_image(img):
     global image_num
 #    image_string = image_bytes.decode('utf-8')
-    nparr = np.fromstring(image_string, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    cv2.imwrite("assets/LiveImage" + str(image_num) + ".png", img)
+#    nparr = np.fromstring(image_string, np.uint8)
+#    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#    cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+#    print(image_num)
+    cv2.imwrite("assets/img/image_" + str(image_num) + ".png", img)
     image_num += 1
 
-def sock_recv():
+def video_thread():
+    global x
+    cap = cv2.VideoCapture(0)
+    time.sleep(1)
     while True:
-        temp = sock.recv(1000000)
-        save_image(temp)
+    	ret, frame = cap.read()
+    	save_image(frame)
+    	time.sleep(1)
+    	stuff = 5/x
+    cap.release()
+    cv2.destroyAllWindows()
 
-sock_thread = threading.Thread(target=sock_recv)
-sock_thread.start()
+#vid_thread = threading.Thread(target=video_thread)
+#vid_thread.start()
 
 app = Flask("__name__", static_folder="assets")
 
@@ -41,9 +52,16 @@ CORS(app)
 def index():
     return render_template("index.html")
 
+@app.route('/receiver')
+def receiver():
+	data = request.get_json()
+	print(data)
+
 @app.route("/data")
 def data():
-    print('hi')
-    return 'hi'
+    global image_num
+    return jsonify({"highest":image_num})
+
 if __name__ == "__main__":
     app.run()
+    x = 0
--- ManualDetection/templates/index.js
+++ ManualDetection/templates/index.js
@@ -1,187 +1,40 @@
-var IMG;
-var subCount;
 //const COMMS_SOCKET = new WebSocket('ws://127.0.0.1:5000')
+const IMG_FILENAME = 'image_';
+const IMG_EXTENSION = '.PNG';
+var IMG;
+var currNum = 0;
+var lowest = 0;
+var highest = 0;
 
 function init(){
-  subCount = 0;
-  SHAPE_OPTIONS = ["Circle", "Semicircle", "Quarter_circle", "Triangle", "Square", "Rectangle", "Trapezoid", "Pentagon", "Hexagon", "Heptagon", "Octagon", "Star", "Cross"]
-  COLOR_OPTIONS = ["Black", "Gray", "White", "Red", "Blue", "Green", "Brown", "Orange", "Yellow", "Purple"]
-  ALPHA_OPTIONS = []
-  for(let i = 0; i < 26; i++){
-    ALPHA_OPTIONS.push(String.fromCharCode(i+65));
-  }
-  for(let i = 0; i < 10; i++){
-    ALPHA_OPTIONS.push(String.fromCharCode(i+48));
-  }
-  resetDropdowns();
+  currNum = 0;
+  lowest = 0;
+  highest = 20;
+//  IMG = document.getElementById('myImage');
+//  IMG.src = 'assets/img/' + IMG_FILENAME + currNum + IMG_EXTENSION;
+}
 
-  IMG = document.getElementById('myImage');
-  IMG.onmousedown = startRubber;
-  IMG.onmouseup = stopRubber;
+function previous(){
+  if(currNum > lowest){
+    currNum -= 1;
+    IMG.src = '../assets/img/' + IMG_FILENAME + currNum + IMG_EXTENSION;
+  }
+}
+
+function send(){
+  console.log('hi');
+  sendData({"num":currNum});
+}
+
+function next(){
+  if(currNum < highest){
+    currNum += 1;
+    img_filename = 'assets/img/' + IMG_FILENAME + currNum + IMG_EXTENSION;
+  }
 }
 
 init();
 
-function openTab(evt, tabName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-  // Get all elements with class="tabcontent" and hide them
-  console.log(tabName);
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-function addToDropdown(select, option_text, option_value){
-  let option = document.createElement('option');
-  option.appendChild(document.createTextNode(option_text));
-  option.value = option_value;
-  select.appendChild(option);
-}
-
-function resetDropdowns(){
-  var shape_dropdown = document.getElementById("shape_dropdown");
-  for(let i = 0; i < SHAPE_OPTIONS.length; i++){
-    addToDropdown(shape_dropdown, SHAPE_OPTIONS[i], SHAPE_OPTIONS[i].toUpperCase());
-  }
-
-  var alpha_dropdown = document.getElementById("alpha_dropdown");
-  for(let i = 0; i < ALPHA_OPTIONS.length; i++){
-    addToDropdown(alpha_dropdown, ALPHA_OPTIONS[i], ALPHA_OPTIONS[i].toUpperCase());
-  }
-  var shape_color_dropdown = document.getElementById("shape_color_dropdown");
-  var alpha_color_dropdown = document.getElementById("alpha_color_dropdown");
-  for(let i = 0; i < COLOR_OPTIONS.length; i++){
-    addToDropdown(shape_color_dropdown, COLOR_OPTIONS[i], COLOR_OPTIONS[i].toUpperCase());
-    addToDropdown(alpha_color_dropdown, COLOR_OPTIONS[i], COLOR_OPTIONS[i].toUpperCase());
-  }
-}
-
-function getClosestOrientation(val){
-  let dirs = ["n", "ne", "e", "se", "s", "sw", "w", "nw"];
-  let closestRotation = Infinity;
-  let closestIndex = 0;
-  for(let index = 0; index < dirs.length; index++){
-    let dir = dirs[index];
-    let dist = Math.min(Math.abs(index * 45 - val), Math.abs(index * 45 + 360 - val));
-    if(dist < closestRotation){
-      closestRotation = dist;
-      closestIndex = index;
-    }
-  }
-  return dirs[closestIndex];
-}
-
-function submit_standard(){
-  let slide = document.getElementById("slide");
-  let shape_dropdown = document.getElementById("shape_dropdown");
-  let shape_color_dropdown = document.getElementById("shape_color_dropdown");
-  let alpha_dropdown = document.getElementById("alpha_dropdown");
-  let alpha_color_dropdown = document.getElementById("alpha_color_dropdown");
-  let
-
-
-  View = document.getElementById('cropView');
-  let dict = {};
-  dict['type'] = 'STANDARD';
-  dict['latitude'] = 0;
-  dict['longitude'] = 0;
-//  dict['orientation'] = getClosestOrientation(slide.value + gps["orientation"]);
-  dict['orientation'] = getClosestOrientation(slide.value);
-  dict['shape'] = shape_dropdown.value;
-  dict['shapeColor'] = shape_color_dropdown.value;
-  dict['alphanumeric'] = alpha_dropdown.value;
-  dict['alphanumericColor'] = alpha_color_dropdown.value;
-  dict['autonomous'] = false;
-  add_submission(dict);
-}
-
-function add_submission(dict, image){
-  let div = document.createElement("div");
-  div.id = "subDiv" + subCount;
-  div.style.padding = "10 10px";
-
-  let subHeader = document.createElement("header");
-  subHeader.textContent = 'Submission #' + subCount;
-
-  let subImg = document.createElement("img");
-  subImg.src = document.getElementById("cropView").src;
-  subImg.width = 100;
-  subImg.height = 100;
-  subImg.style.float = "left";
-
-  let textDiv = document.createElement("div");
-  textDiv.classList.add("submissionRow");
-
-  let shapeDiv = document.createElement("div");
-  shapeDiv.classList.add("column");
-  shapeDiv.style.backgroundColor = "aaa";
-  shapeDiv.id = "shapeDiv";
-
-  let shapeHeader = document.createElement("h3");
-  shapeHeader.textContent = "Shape:";
-
-  let shapeClass = document.createElement("p");
-  shapeClass.setAttribute('style', 'white-space: pre;');
-  shapeClass.textContent = 'Classification: ' + dict['shape'] + '\r\n       Color: ' + dict['shapeColor'];
-
-  let alphaDiv = document.createElement("div");
-  alphaDiv.classList.add("column");
-  alphaDiv.style.backgroundColor = "bbb";
-
-  let alphaHeader = document.createElement("h3");
-  alphaHeader.textContent = "Alphanumeric:";
-
-  let alphaClass = document.createElement("p");
-  alphaClass.setAttribute('style', 'white-space: pre;');
-  alphaClass.textContent = 'Classification: ' + dict['alphanumeric'] + '\r\n';
-  alphaClass.textContent += 'Color: ' + dict['alphanumericColor'];
-
-
-
-  shapeDiv.appendChild(shapeHeader);
-  shapeDiv.appendChild(shapeClass);
-
-  alphaDiv.appendChild(alphaHeader);
-  alphaDiv.appendChild(alphaClass);
-
-  textDiv.appendChild(shapeDiv);
-  textDiv.appendChild(alphaDiv);
-
-  div.appendChild(subHeader);
-  div.appendChild(subImg);
-  div.appendChild(textDiv);
-  document.getElementById("Submissions").appendChild(div);
-  subCount++;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 var updateData = function() {
     fetch("http://localhost:5000/data")
         .then(function(response) {
@@ -189,11 +42,22 @@ var updateData = function() {
         })
         .then(function(data) {
             // Update the DOM
-
+            highest = data['highest'];
+            console.log(highest);
         })
         .catch(function() {
             console.log("Error occured with data request");
         });
 };
-*/
-//setInterval(updateData, 5);
+
+var doWork = function(){
+  // ajax the JSON to the server
+	$.post("receiver", data, function(){
+
+	});
+	// stop link reloading the page
+ event.preventDefault();
+}
+
+setInterval(updateData, 1000);
+//setInterval(sendData, 1000);
