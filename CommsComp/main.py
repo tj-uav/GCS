50df7a21c54ae152e74a302c7babc33b76126dbf
Srikar <srikarg89@gmail.com>
2019-05-25 11:07:37 -0400

    Fixed comms comp errors

--- CommsComp/main.py
+++ CommsComp/main.py
@@ -8,9 +8,6 @@ import requests
 import cv2
 import os
 
-# Self-declared package imports
-from ../helpers/comms_help import _decode_list, _decode_dict, video_capture
-
 # AUVSI imports
 from auvsi_suas.client import client
 from auvsi_suas.proto import interop_api_pb2
@@ -26,8 +23,12 @@ MISSION_ID = 1
 #BASE_URL = "http://localhost:8000/api/"
 ODCL_IDS = {}
 ALLACTIONS = {'GET': ['MISSION', 'ALLODCLS', 'ODCL', 'ODCLIMAGE'], 'POST': ['ODCL', 'TELEMETRY'], 'PUT': ['ODCL', 'ODCLIMAGE'], 'DELETE': ['ODCL, ODCLIMAGE']}
-global client, saved_image_num
-client = None
+ODCL_SHAPECONV = {'CIRCLE' : 1, 'SEMICRICLE' : 2, 'QUARTER_CIRCLE' : 3, 'TRIANGLE' : 4, 'SQUARE' : 5, 'RECTANGLE' : 6, 'TRAPEZOID' : 7, 'PENTAGON' : 8, 'HEXAGON' : 9, 'HEPTAGON' : 10, 'OCTAGON' : 11, 'STAR' : 12, 'CROSS' : 13}
+ODCL_COLORCONV = {'WHITE' : 1, 'BLACK' : 2, 'GRAY' : 3, 'RED' : 4, 'BLUE' : 5, 'GREEN' : 6, 'YELLOW' : 7, 'PURPLE' : 8, 'BROWN' : 9, 'ORANGE' : 10}
+ODCL_ORIENTATIONCONV = {'N' : 1, 'NE' : 2, 'E' : 3, 'SE' : 4, 'S' : 5, 'SW' : 6, 'W' : 7, 'NW' : 8}
+
+global cl, saved_image_num
+cl = None
 saved_image_num = 0
 
 def start():
@@ -48,6 +49,53 @@ def start():
 #    vid_thread = threading.Thread(target=video_capture)
 #    vid_thread.start()
 
+import requests
+import json
+
+def _decode_list(data):
+    rv = []
+    for item in data:
+        if isinstance(item, str):
+            item = item.encode('utf-8')
+        elif isinstance(item, list):
+            item = _decode_list(item)
+        elif isinstance(item, dict):
+            item = _decode_dict(item)
+        rv.append(item)
+    return rv
+
+def _decode_dict(data):
+    rv = {}
+    for key, value in data.iteritems():
+        if isinstance(key, str):
+            key = key.encode('utf-8')
+        if isinstance(value, str):
+            value = value.encode('utf-8')
+        elif isinstance(value, list):
+            value = _decode_list(value)
+        elif isinstance(value, dict):
+            value = _decode_dict(value)
+        rv[key] = value
+    return rv
+
+#Temporary method
+def video_capture():
+    cap = cv2.VideoCapture(0)
+    time.sleep(1)
+    while True:
+        ret, frame = cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        nparr = cv2.imencode('.jpg',frame)[1]
+        temp = nparr.tolist()
+        print("Numpy shape:",nparr.shape)
+#        img_bytes = base64.b64decode(temp).decode('utf-8')
+        img_bytes = temp
+        print(type(img_bytes))
+        enqueue(destination=IPS['MANUAL_DETECTION'],header='CAMERA_IMAGE',message=img_bytes)
+        time.sleep(0.5)
+    cap.release()
+    cv2.destroyAllWindows()
+
 def connect_device(sock):
     conn, addr = sock.accept()
     listen_thread = threading.Thread(target=listen_from_device, args=(conn,))
@@ -56,12 +104,12 @@ def connect_device(sock):
     CONNECTIONS.append((ip, conn))
 
 def connect_interop(interop_url, username, password):
-    global client
-    client = client.Client(url='http://127.0.0.1:8000',
+    global cl
+    cl = client.Client(url='http://127.0.0.1:8000',
                        username='testuser',
                        password='testpass')
-
-def make_odlc_from_data():
+"""
+def make_odlc_from_data(message_data, odlc_id):
     odlc = interop_api_pb2.Odlc()
     if 'type' in message_data and type(message_data['type']) == str:
         odlc.type = message_data['type']
@@ -78,10 +126,12 @@ def make_odlc_from_data():
     if 'alphanumeric' in message_data and type(message_data['alphanumeric']) == str:
         odlc.alphanumeric = message_data['alphanumeric']
     if 'alphanumeric_color' in message_data and message_data['alphanumeric_color'] in ODCL_COLORCONV:
-        odlc.alphanumeric_color =ODCL_COLORCONV[ message_data['alphanumeric_color']]
+        odlc.alphanumeric_color = ODCL_COLORCONV[ message_data['alphanumeric_color']]
     return odlc
 
+
 def interop_handler(action, subaction, message_data=None):
+    global cl
     if action not in ALLACTIONS:
         print("Action not found")
         return
@@ -95,9 +145,9 @@ def interop_handler(action, subaction, message_data=None):
         assert(type(message_data) == dict)
         if subaction == 'ODCL':
             odlc = make_odlc_from_data(odlc)
-            odlc = client.post_odlc(odlc)
+            odlc = cl.post_odlc(odlc)
             if 'image_data' in message_data and type(message_data['image_data']) == bytes:
-                client.put_odlc_image(odlc.id, message_data['image_data'])
+                cl.put_odlc_image(odlc.id, message_data['image_data'])
             return odlc
         elif subaction == 'TELEMETRY':
             telemetry = interop_api_pb2.Telemetry()
@@ -110,7 +160,7 @@ def interop_handler(action, subaction, message_data=None):
             telemetry.longitude = message_data['longitude']
             telemetry.altitude = message_data['altitude']
             telemetry.heading = message_data['heading']
-            client.post_telemetry(telemetry)
+            cl.post_telemetry(telemetry)
     elif action == 'PUT':
         if message_data is None:
             print('Missing message data')
@@ -121,17 +171,17 @@ def interop_handler(action, subaction, message_data=None):
         assert(type(message_data) == dict)
         odlc_id = message_data['ODCL_ID']
         if subaction == 'ODCL':
-            odlc = make_odlc_from_data(odlc_id, odlc)
-            odlc = client.put_odlc(odlc)
+            odlc = make_odlc_from_data(message_data=message_data, odlc_id=odlc_id)
+            odlc = cl.put_odlc(odlc)
             return odlc
         elif subaction == 'ODCLIMAGE':
             if 'image_data' in message_data and type(message_data['image_data']) == bytes:
-                return client.put_odlc_image(odlc_id, message_data['image_data'])
+                return cl.put_odlc_image(odlc_id, message_data['image_data'])
     elif action == 'GET':
         if subaction == 'MISSION':
-            return client.get_mission(MISSION_ID)
+            return cl.get_mission(MISSION_ID)
         elif subaction == 'ALLODCLS':
-            return client.get_odlcs(mission=MISSION_ID)
+            return cl.get_odlcs(mission=MISSION_ID)
         else:
             if message_data is None:
                 print('Missing message data')
@@ -142,9 +192,9 @@ def interop_handler(action, subaction, message_data=None):
             assert(type(message_data) == dict)
             odlc_id = message_data['ODCL_ID']
             if subaction == 'ODCL':
-                return client.get_odlc(odlc_id)
+                return cl.get_odlc(odlc_id)
             elif subaction == 'ODCLIMAGE':
-                return client.get_odlc_image(odlc_id)
+                return cl.get_odlc_image(odlc_id)
     elif action == 'DELETE':
         if message_data is None:
             print('Missing message data')
@@ -155,10 +205,10 @@ def interop_handler(action, subaction, message_data=None):
         assert(type(message_data) == dict)
         odlc_id = message_data['ODCL_ID']
         if subaction == 'ODCL':
-            client.delete_odlc(odlc_id)
+            cl.delete_odlc(odlc_id)
         elif subaction == 'ODCLIMAGE':
-            client.delete_odlc_image(odlc_id)
-
+            cl.delete_odlc_image(odlc_id)
+"""
 def enqueue(destination, header, message, subheader = None):
 #    print(message)
     to_send = {}
