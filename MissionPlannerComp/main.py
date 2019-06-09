d03971044c2ebdcb4117e5ed22ad28c69d885462
Srikar Gouru <srikarg89@gmail.com>
2019-06-08 21:07:14 -0400

    Working telemetry submission

--- MissionPlannerComp/main.py
+++ MissionPlannerComp/main.py
@@ -1,21 +1,3 @@
-import sys
-#print(sys.path)
-#from google.protobuf import json_format
-sys.path.append("C:/Python27/Lib")
-sys.path.append("C:/Python27/Lib/site-packages")
-sys.path.append("C:/Python27/Lib/site-packages/google")
-sys.path.append("C:/Python27/Lib/site-packages/google/protobuf")
-sys.path.append("E:/Jason/UAV/interop/client")
-#import google.protobuf.json_format
-#to_import = [ 'E:/Jason/UAV/interop/client', 'C:/WINDOWS/SYSTEM32/python27.zip', 'C:/Python27/DLLs', 'C:/Python27/lib',
-#    'C:/Python27/lib/plat-win', 'C:/Python27/lib/lib-tk', 'C:/Python27', 'C:/Python27/lib/site-packages']
-#for path in to_import:
-#    sys.path.append(path)
-print('Started importing')
-print(sys.path)
-#import google
-#import json_format
-#from google.protobuf import json_format
 import geopy
 import geopy.distance
 import socket
@@ -23,11 +5,10 @@ import threading
 import time
 from collections import deque   
 import json
-print('Almost done')
 
+#External imports
 from auvsi_suas.client import client
 from auvsi_suas.proto import interop_api_pb2
-print('Done Importing')
 
 #from auvsi_suas.client import client
 #from auvsi_suas.proto import interop_api_pb2
@@ -40,52 +21,18 @@ NUM_OBSTACLE_POINTS = 10
 MILES_TO_KILOMETERS = 0.62137119  # Miles to kilometers
 
 MY_IP = '127.0.0.1'
-ODCL_IP = '127.0.0.1'
 PORT = 5005
-MESSAGE_QUEUE = deque([])
 MISSION_ID = 1
 global x
 x = 5
 
 def start():
-    connect_interop("http://98.169.139.31:8000", "testuser", "testpass")
+#    connect_interop("http://98.169.139.31:8000", "testuser", "testpass")
+    connect_interop("http://192.168.0.27:8000", "testuser", "testpass")
+    connect_comms()
     print('Connected')
     process_mission_data()
     print('Created')
-
-#HELPER METHODS
-def _decode_list(data):
-    rv = []
-    for item in data:
-        if isinstance(item, list):
-            item = _decode_list(item)
-        elif isinstance(item, dict):
-            item = _decode_dict(item)
-        else:
-            item = item.encode('utf-8')
-        rv.append(item)
-    return rv
-
-def _decode_dict(data):
-    rv = {}
-    for key, value in data.iteritems():
-        if isinstance(value, list):
-            value = _decode_list(value)
-        elif isinstance(value, dict):
-            value = _decode_dict(value)
-        else:
-            key = key.encode('utf-8')
-            value = key.encode('utf-8')
-#        if isinstance(key, unicode):
-#            key = key.encode('utf-8')
-#        if isinstance(value, unicode):
-#            value = value.encode('utf-8')
-#        elif isinstance(value, list):
-#            value = _decode_list(value)
-#        elif isinstance(value, dict):
-#            value = _decode_dict(value)
-        rv[key] = value
-    return rv
 
 def connect_interop(interop_url, username, password):
     global cl
@@ -93,43 +40,15 @@ def connect_interop(interop_url, username, password):
                        username=username,
                        password=password)
 
-"""
 def connect_comms():
-    global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Over Internet, TCP protocol
-    sock.connect((COMMS_IP, PORT))
+    sock.bind((MY_IP, PORT))
+    sock.listen(1)
 
-    listen_thread = threading.Thread(target=listen_from_device, args=(sock,))
+    conn, addr = sock.accept()
+
+    listen_thread = threading.Thread(target=listen_from_device, args=(conn,))
     listen_thread.start()
-
-    send_thread = threading.Thread(target=send_data, args=(sock,))
-    send_thread.start()
-
-    time.sleep(2)
-    print('Requesting Mission')
-    enqueue(destination=COMMS_IP, header='INTEROP', subheader='GET MISSION', message={})
-
-#    telem_data_thread = threading.Thread(target=telem_data)
-#    telem_data_thread.start()
-    time.sleep(4)
-    global x
-    x = 0
-    sock.close()
-    exit()
-"""
-
-def telem_data():
-    global cl
-    while True:
-        telemetry = interop_api_pb2.Telemetry()
-        telemetry.altitude = cs.lat
-        telemetry.longitude = cs.lng
-        telemetry.alt = cs.alt
-        telemetry.heading = cs.yaw
-#        enqueue(destination=COMMS_IP, header='TELEMETRY_DATA', message=telem)
-        cl.post_telemetry(telemetry)
-        print("Sleeping for 0.1 seconds...")
-        time.sleep(.1)
 
 #Radius assumes feet
 def circleToPoints(centerx, centery, radius, num_points=40):
@@ -256,12 +175,11 @@ def process_mission_data():
 #    open_file(filename)
 #    create_file(filename,obstacles_waypoints)
 
-"""
 def listen_from_device(sock):
     #Constantly be listening
     #Upon receiving a message, pass it through the command_ingest
     while True:
-        data_bytes = sock.recv(10240)
+        data_bytes = sock.recv(1024)
         data_string = data_bytes.decode("utf-8")
         data_dict = json.loads(data_string)
         ingest_thread = threading.Thread(target=command_ingest, args=(data_dict,))
@@ -274,32 +192,15 @@ def command_ingest(message_dict):
     #Interpret messages based on header and other stuff
     #Examples include: Changing the buffer value, forwarding messages to other devices, submitting to interop, etc.
     header = message_dict['HEADER']
-    if header == 'MISSION_DATA':
-        process_mission_data(message_dict['MESSAGE'])
+    if header == 'TELEMETRY':
+        msg = message_dict['MESSAGE']
+        telemetry = interop_api_pb2.Telemetry()
+        telemetry.latitude = msg['lat']
+        telemetry.longitude = msg['lng']
+        telemetry.altitude = msg['alt']
+        telemetry.heading = msg['head']
+        cl.post_telemetry(telemetry)
 
-def enqueue(destination, header, message, subheader = None):
-    to_send = {}
-    to_send['SOURCE'] = MY_IP
-    to_send['DESTINATION'] = destination
-    to_send['HEADER'] = header
-    to_send['MESSAGE'] = message
-    if subheader:
-        to_send['SUBHEADER'] = subheader
-    MESSAGE_QUEUE.append(to_send)
 
-def send_data(sock):
-    #Check if MESSAGE_QUEUE is empty. If it is not empty, send that message to the corresponding device
-    while True:
-        if MESSAGE_QUEUE:
-            nextMessage = MESSAGE_QUEUE.popleft()
-            DESTINATION_IP = nextMessage['DESTINATION']
-            nextMessage_json = json.dumps(nextMessage)
-            nextMessage_bytes = nextMessage_json.encode('utf-8')
-            sock.send(nextMessage_bytes)
-            time.sleep(0.05) #Can be changed
-            global x
-            if x == 0:
-                return
-"""
 if __name__ == '__main__':
     start()
---
+++ MissionPlannerComp/missionplanner.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.append("C:/Python27/Lib")
+sys.path.append("C:/Python27/Lib/site-packages")
+print('Started importing')
+
+import socket
+import threading
+import time
+from collections import deque   
+import json
+import random
+
+print('Done importing')
+
+MY_IP = '127.0.0.1'
+PORT = 5005
+MESSAGE_QUEUE = deque([])
+MISSION_ID = 1
+global x
+x = 5
+
+def start():
+    connect_comms()
+    print('Connected')
+    telem_data_thread = threading.Thread(target=telem_data)
+    telem_data_thread.start()
+    while True:
+        pass
+
+def connect_comms():
+    global sock
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Over Internet, TCP protocol
+    sock.connect((MY_IP, PORT))
+
+    send_thread = threading.Thread(target=send_data, args=(sock,))
+    send_thread.start()
+
+def telem_data():
+    global cl
+    while True:
+        packet = {}
+#        packet['lat'] = cs.lat
+#        packet['lng'] = cs.lng
+#        packet['alt'] = cs.alt
+#        packet['head'] = cs.yaw
+        packet['lat'] = random.random() * 180
+        packet['lng'] = random.random() * 90
+        packet['alt'] = random.randrange(100,200)
+        packet['head'] = random.randrange(0,360)
+        enqueue(header='TELEMETRY', message=packet)
+        print("Sleeping for 0.1 seconds...")
+        time.sleep(.1)
+
+def enqueue(header, message, subheader = None):
+    to_send = {}
+    to_send['SOURCE'] = MY_IP
+    to_send['HEADER'] = header
+    to_send['MESSAGE'] = message
+    if subheader:
+        to_send['SUBHEADER'] = subheader
+    MESSAGE_QUEUE.append(to_send)
+
+def send_data(sock):
+    #Check if MESSAGE_QUEUE is empty. If it is not empty, send that message to the corresponding device
+    while True:
+        if MESSAGE_QUEUE:
+            nextMessage = MESSAGE_QUEUE.popleft()
+            nextMessage_json = json.dumps(nextMessage)
+            nextMessage_bytes = nextMessage_json.encode('utf-8')
+            sock.send(nextMessage_bytes)
+            time.sleep(0.05) #Can be changed
+            global x
+            if x == 0:
+                return
+
+if __name__ == '__main__':
+    start()
