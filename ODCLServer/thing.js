4fcfc2b3ee6fa12b4d50adacf8cb0c83636b027e
Aryan34 <aryanc2021@gmail.com>
2019-11-23 13:56:03 -0500

    ODCL server

---
+++ ODCLServer/thing.js
@@ -0,0 +1,5 @@
+var newSubmission = document.createElement("div");
+var img = new Image()
+img.src = 'submit.jpg'
+newSubmission.className = "submission"
+document.getElementById("s1").appendChild(newSubmission);
