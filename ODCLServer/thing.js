bc5206e6ed985dd9d881dd24907b3d49412591e1
Jason Klein <kleinjason234@gmail.com>
2019-11-23 14:28:35 -0500

    add addSubmission() method, HTML structure is sensible

--- ODCLServer/thing.js
+++ ODCLServer/thing.js
@@ -1,5 +1,17 @@
-var newSubmission = document.createElement("div");
-var img = new Image()
-img.src = 'submit.jpg'
-newSubmission.className = "submission"
-document.getElementById("s1").appendChild(newSubmission);
+var submissions = 0
+
+var parent = document.createElement("div")
+parent.classList.add("submission")
+document.getElementById("submissions").appendChild(parent)
+
+function addSubmission(image_path) {
+	let image = document.createElement("img")
+	image.src = `${image_path}`
+	parent.appendChild(image)
+
+	let caption = document.createElement("figcaption")
+	caption.textContent = `Submission ${++submissions}`
+	parent.appendChild(caption)
+}
+
+addSubmission("submit.jpg")
