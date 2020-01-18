4eb97548459661c57254cf78cdbde77df1ac1fbd
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2020-01-18 12:59:25 -0500

    Test push

--- interop_test.py
+++ interop_test.py
@@ -1,6 +1,8 @@
 from auvsi_suas.client import client
 from auvsi_suas.proto import interop_api_pb2
 
+# Test commit 1/18/2020
+
 cl = client.Client(url='http://192.168.1.38:8000',
                        username='testuser',
                        password='testpass')
