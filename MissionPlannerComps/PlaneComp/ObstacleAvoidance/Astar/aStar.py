a7c91a90e529c9bc0320c1c5e0a29bb47da5d936
Srikar Gouru <srikarg89@gmail.com>
2020-02-09 00:28:55 -0500

    Neater code

--- MissionPlannerComps/PlaneComp/ObstacleAvoidance/Astar/aStar.py
+++ MissionPlannerComps/PlaneComp/ObstacleAvoidance/Astar/aStar.py
@@ -10,12 +10,13 @@ import json
 from mp_help import make_kml_file
 
 fig, ax = plt.subplots()
+ax.set_aspect(1)
 
 # base_lat = 38.147250  # base coords from AUVSI rules
-# base_long = -76.426444
+# base_lon = -76.426444
 
 base_lat = 38.8355249
-base_long = -77.505455
+base_lon = -77.505455
 
 # waypoints + obstacle_list
 waypoint_x = []
@@ -24,7 +25,6 @@ waypoint_z = []
 waypoint_path_x = []
 waypoint_path_y = []
 waypoint_path_z = []
-obstacle_list = []
 
 # constants in METERS
 # precision is inversely related to speed of algorithm
@@ -45,17 +45,14 @@ MAX_PHI = pi
 class Obstacle():
     # obstacle at x,y with radius r
     def __init__(self, x, y, z, r):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.r = r
+        self.x, self.y, self.z, self.r = x, y, z, r
 
     # for debugging
     def __str__(self):
         return "Center: ({}. {}), Radius: {}".format(round(self.x, 3), round(self.y, 3), round(self.r, 3))
 
     def inMe(self, x, y, z):
-        return (y - self.y) ** 2 + (x - self.x) ** 2 < ((self.r + radius_tolerance) ** 2) and (self.z + height_tolerance) >= z
+        return (y - self.y) ** 2 + (x - self.x) ** 2 < ((self.r + radius_tolerance) ** 2) and z < (self.z + height_tolerance)
 
     # return matplotlib friendly shape
     def plottable(self):
@@ -85,63 +82,47 @@ class Node():
     def __str__(self):
         return str(self.x) + ", " + str(self.y) + ", " + str(self.z)
 
+    def check_collision(self, node, obs):
+        for o in obs:
+            if o.inMe(node.x, node.y, node.z):
+                return True
+        return False
+
     def nbrs(self, obs):
         global dPhi, dTheta
-        lst = []
-        for dp in np.arange(-pi / 12, pi / 12 + dPhi, dPhi):
-            if self.phi + dp > MAX_PHI or self.phi + dp < MIN_PHI: continue
+        poss = set()
+#        for dp in np.arange(-pi / 12, pi / 12 + dPhi, dPhi):
+#            if self.phi + dp > MAX_PHI or self.phi + dp < MIN_PHI: continue
+        if True:
+            dp = 0
             for dt in np.arange(-pi / 6, pi / 6 + dTheta, dTheta):
-                lst.append(Node(self.x + rho * cos(self.theta + dt) * sin(self.phi + dp),
+                node = Node(self.x + rho * cos(self.theta + dt) * sin(self.phi + dp),
                                 self.y + rho * sin(self.theta + dt) * sin(self.phi + dp),
-                                self.z + rho * cos(self.phi + dp), self, self.theta + dt, self.phi + dp))
-        lst = list(set(lst))
-        for o in obs:
-            for n in lst:
-                if o.inMe(n.x, n.y, n.z):
-                    lst.remove(n)
-        return lst
+                                self.z + rho * cos(self.phi + dp), self, self.theta + dt, self.phi + dp)
+                if not self.check_collision(node, obs):
+                    poss.add(node)
+
+        return poss
 
     def __eq__(self, n):
-        global rho
         return self.dist(n) < rho
 
     def __lt__(self, n):
         return self.f < n.f
 
-
-def tempPlot(node):
-   arrX = []
-   arrY = []
-   while node:
-      arrX.append(node.x)
-      arrY.append(node.y)
-      node = node.parent
-   arrX,arrY = arrX[::-1], arrY[::-1]
-   path = plt.plot(arrX, arrY, 'bo', label="Path")
-   fig.canvas.draw()
-   time.sleep(0.1)
-   l = path.pop(0)
-   l.remove()
-   del l
-
-
-def aStar(root, goal):
-    global waypoints, rho
-    root.parent = None
+def aStar(root, goal, obstacles):
     f = root.dist(goal)
     openSet = [root]
     path = []
-    waypoints.remove(goal.loc())
     closedSet = set()
     num = 0
     while True:
         node = heapq.heappop(openSet)
-        tempPlot(node)
         if node in closedSet:
             continue
         closedSet.add(node)
-        for nbr in node.nbrs(obstacle_list):
-            goal.parent = node
+        for nbr in node.nbrs(obstacles):
+            nbr.parent = node
             if nbr == goal:
                 nbr.x = goal.x
                 nbr.y = goal.y
@@ -150,28 +131,20 @@ def aStar(root, goal):
                     # if nbr.theta != nbr.parent.theta or nbr.phi != nbr.parent.phi:
                     path.append(nbr.loc())
                     nbr = nbr.parent
+                
+                return path
 
-                waypoints.extend(path[::-1])
-                waypoints.append(goal.loc())
-                return
             nbr.setF(nbr.dist(goal) - node.dist(goal) + f)
             heapq.heappush(openSet, nbr)
+    
+    return path
 
 
 def read_mission():
-    global waypoints, obstacle_list, forKML
-    forKML = []
-    # import requests
-    # s = requests.Session()
-    # url = "http://192.168.1.43:8000/api/"
-    # params = {"username": "testadmin", "password": "testpass"}
-    # id = 7
-
-    # r = s.post(url+"login", json=params)
-    # r = s.get(url+"missions/"+str(id))
+    geo_obstacles = []
+    obstacles = []
     r = open('mission.txt', 'r').read()
     mission_dict = json.loads(r)
-    # mission_dict = json.loads(r.text)
 
     for obstacle in mission_dict["stationaryObstacles"]:
         lat = obstacle["latitude"]
@@ -179,44 +152,52 @@ def read_mission():
         height = obstacle["height"]
         rad = obstacle["radius"] * 0.3048  # feet to meters
 
-        forKML += [[lat, lon, rad]]
-        dx = (base_long - lon) * cos((base_lat + lat) * pi / 360) * 40000000 / 360  # horiz. dist from base
-        dy = (base_lat - lat) * 40000000 / 360  # vert. distance from base (meters)
+        geo_obstacles.append([lat, lon, rad])
+#        dx = (base_lon - lon) * cos((base_lat + lat) * pi / 360) * 40000000 / 360  # horiz. dist from base
+        dx = (base_lon - lon) * 40000000 / 360
+        dy = (base_lat - lat) * 40000000 / 360
 
-        obstacle_list += [Obstacle(dx, dy, height, rad)]
-        ax.add_artist(Obstacle(dx, dy, height, rad).plottable())
+        obstacles.append(Obstacle(dx, dy, height, rad))
 
     waypoints = []
-    og_waypoints = []
+    geo_waypoints = []
     for waypoint in mission_dict["waypoints"]:
         lat = waypoint["latitude"]
         lon = waypoint["longitude"]
         alt = waypoint["altitude"]
 
-        dx = (base_long - lon) * 40000000 * cos((base_lat + lat) * pi / 360) / 360
+#        dx = (base_lon - lon) * 40000000 * cos((base_lat + lat) * pi / 360) / 360
+        dx = (base_lon - lon) * 40000000 / 360
         dy = (base_lat - lat) * 40000000 / 360
 
-        og_waypoints.append([lat, lon, alt])
+        geo_waypoints.append([lat, lon, alt])
         waypoints.append([dx, dy, alt])
 
-        if len(waypoints)>1:
+        if len(waypoints) > 1:
             break
-    return og_waypoints
+
+    return geo_waypoints, waypoints, geo_obstacles, obstacles
 
 
-def generate_final_path():
-    global waypoints
-    for i in range(len(waypoints)):
+def generate_final_path(waypoints, obstacles):
+    paths = []
+    root = Node(*waypoints[0], None)
+    for i in range(1, len(waypoints)):
         goal = Node(*waypoints[i], None)
-        root = Node(*waypoints[i - 1], goal)
-        aStar(root, goal)
+#        root = Node(*waypoints[i - 1], None)
+        path = aStar(root, goal, obstacles)
+        paths.append(path)
+        root = path[-1]
 
     final_path = []
-    for coordinate in waypoints:
-        reversed_lat = -(coordinate[1] * 360 / 40000000 - base_lat)
-        reversed_lon = -(coordinate[0] * 360 / 40000000 / cos((base_lat + reversed_lat) * pi / 360) - base_long)
+    for path in paths:
+        for coordinate in path:
+            final_path.append(coordinate)
+#    for coordinate in waypoints:
+#        reversed_lat = -(coordinate[1] * 360 / 40000000 - base_lat)
+#        reversed_lon = -(coordinate[0] * 360 / 40000000 / cos((base_lat + reversed_lat) * pi / 360) - base_lon)
 
-        final_path.append((reversed_lat, reversed_lon, coordinate[2]))
+#        final_path.append((reversed_lat, reversed_lon, coordinate[2]))
     return final_path
 
 
@@ -238,36 +219,50 @@ def writeFile(filename, path):
     write.close()
 
 
-def display():
-    global waypoints
+def display(path, waypoints, obstacles):
+    path_x, path_y, path_z = [], [], []
+    for waypoint in path:
+        path_x.append(waypoint[0])
+        path_y.append(waypoint[1])
+        path_z.append(waypoint[2])
+
+    waypoint_x, waypoint_y, waypoint_z = [], [], []
     for waypoint in waypoints:
-        waypoint_path_x.append(waypoint[0])
-        waypoint_path_y.append(waypoint[1])
-        waypoint_path_z.append(waypoint[2])
+        waypoint_x.append(waypoint[0])
+        waypoint_y.append(waypoint[1])
+        waypoint_z.append(waypoint[2])
 
-
-def main():
-    global waypoints, forKML
-    og_waypoints = read_mission()
-    writeFile("original.waypoints", og_waypoints)
-    final_path = generate_final_path()
-    writeFile("optimized.waypoints", final_path)
-    make_kml_file("obstacles.kml", obstacles=forKML)
-    # for wp in final_path:
-    #     print(wp)
-    # print(len(final_path), "waypoints")
-    display()
     print("waypoints:")
-    print(waypoint_path_x)
-    print(waypoint_path_y)
+    print(path_x)
+    print(path_y)
 
     plt.ylim(-300, 300)
     plt.xlim(-300, 300)
 
     ax.grid()
-    plt.plot(waypoint_path_x, waypoint_path_y, 'ro', label='Waypoints')
+
+    for obs in obstacles:
+        ax.add_artist(obs.plottable())
+
+    plt.plot(path_x, path_y, 'ro', label='Waypoints')
+    plt.plot(waypoint_x, waypoint_y, 'yo', label='Waypoints')
     plt.legend()
     plt.show()
+
+
+def main():
+    global waypoints, forKML
+    geo_waypoints, waypoints, geo_obstacles, obstacles = read_mission()
+
+#    writeFile("original.waypoints", og_waypoints)
+    final_path = generate_final_path(waypoints, obstacles)
+#    writeFile("optimized.waypoints", final_path)
+#    make_kml_file("obstacles.kml", obstacles=forKML)
+
+    # for wp in final_path:
+    #     print(wp)
+    # print(len(final_path), "waypoints")
+    display(final_path, waypoints, obstacles)
 
 
 if __name__ == '__main__':
